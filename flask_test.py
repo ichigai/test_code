0e6f65b6dda27c157f82849d5cb58f94c5e88559
ichigai <opaqueike@gmail.com>
2024-10-19 12:35:33 +0900

    flaskサーバーのサンプルを追加

---
+++ flask_test.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# /homeにアクセスしたときにindex.htmlを表示
+@app.route('/home')
+def home():
+    return render_template('sample1.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
