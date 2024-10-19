86979bd99edb92c8251d3bbda0fd640d48966e3e
ichigai <opaqueike@gmail.com>
2024-10-19 14:05:41 +0900

    flaskでfoliumを表示してみる

--- flask_test.py
+++ flask_test.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 @app.route('/home')
 def home():
     return render_template('sample1.html')
+@app.route('/folium')
+def folium1():
+    return render_template('folium_test.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
